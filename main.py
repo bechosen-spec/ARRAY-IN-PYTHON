67ec1642a3d61edd8145ef9868f604c92d1d7888
BONIFACE EMMANUEL CHOSEN <65294466+bechosen-spec@users.noreply.github.com>
2022-05-19 12:37:58 +0000

    Added comments and append and pop functions

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+# creating an array in python
 strings= ['a', 'b', 'c', 'd']
 
-strings.append('e')
+strings.append('e') # push() which adds data to the end of the list/array --> O(1)
 
 print(strings)
-strings.pop()
+strings.pop() # remove item from the end of the array --> O(1)
 print(strings)
